741bd3703b47e2b612d69a5b2ede080f119fad56
Samee-Ur-Rehman <sameerehman001>
2025-02-20 18:44:25 +0500

    image optimization

--- src/pages/mainPage/imagesArray.jsx
+++ src/pages/mainPage/imagesArray.jsx
@@ -1,36 +1,102 @@
-import image1 from "../../assets/images/3P.jpg";
-import image2 from "../../assets/images/C1PS.jpg";
-import image3 from "../../assets/images/Interior.png";
-import image4 from "../../assets/images/Livingp.png";
-import image5 from "../../assets/images/112.jpg";
-import image6 from "../../assets/images/Render 1 (1).png";
-import image7 from "../../assets/images/Render 2.png";
-import image8 from "../../assets/images/Render 3.png";
-import image9 from "../../assets/images/Render 4.png";
-import image10 from "../../assets/images/sample 1.png";
-import image11 from "../../assets/images/Mini kitchen 2.png";
-import image12 from "../../assets/images/sample 2.png";
-import image13 from "../../assets/images/Render 9PP.png";
-import image14 from "../../assets/images/Sample 6.png";
-import image15 from "../../assets/images/Sample 8.png";
-import image16 from "../../assets/images/sample 4.png";
-import image17 from "../../assets/images/Kitchen Render.jpeg";
-import image18 from "../../assets/images/Render 111.png";
-import image19 from "../../assets/images/Sample 3.jpg";
-import image20 from "../../assets/images/2P.jpg";
-import image21 from "../../assets/images/Sample 8.png";
-import image22 from "../../assets/images/Sample 51.jpg";
-import image23 from "../../assets/images/Sample render.jpg";
-import image24 from "../../assets/images/Snow_render1.jpeg";
-import image25 from "../../assets/images/Render 9PP.png";
-import image26 from "../../assets/images/sample 4.png";
-import image27 from "../../assets/images/Render 6.png";
-import image28 from "../../assets/images/Sample 8.png";
-import image29 from "../../assets/images/Render 9PP.png";
-import image30 from "../../assets/images/sample 4.png";
-import image31 from "../../assets/images/Render 6.png";
-import image32 from "../../assets/images/Sample 8.png";
+// import image1 from "../../assets/images/3P.jpg";
+// import image2 from "../../assets/images/C1PS.jpg";
+// import image3 from "../../assets/images/Interior.png";
+// import image4 from "../../assets/images/Livingp.png";
+// import image5 from "../../assets/images/112.jpg";
+// import image6 from "../../assets/images/Render 1 (1).png";
+// import image7 from "../../assets/images/Render 2.png";
+// import image8 from "../../assets/images/Render 3.png";
+// import image9 from "../../assets/images/Render 4.png";
+// import image10 from "../../assets/images/sample 1.png";
+// import image11 from "../../assets/images/Mini kitchen 2.png";
+// import image12 from "../../assets/images/sample 2.png";
+// import image13 from "../../assets/images/Render 9PP.png";
+// import image14 from "../../assets/images/Sample 6.png";
+// import image15 from "../../assets/images/Sample 8.png";
+// import image16 from "../../assets/images/sample 4.png";
+// import image17 from "../../assets/images/Kitchen Render.jpeg";
+// import image18 from "../../assets/images/Render 111.png";
+// import image19 from "../../assets/images/Sample 3.jpg";
+// import image20 from "../../assets/images/2P.jpg";
+// import image21 from "../../assets/images/Sample 8.png";
+// import image22 from "../../assets/images/Sample 51.jpg";
+// import image23 from "../../assets/images/Sample render.jpg";
+// import image24 from "../../assets/images/Snow_render1.jpeg";
+// import image25 from "../../assets/images/Render 9PP.png";
+// import image26 from "../../assets/images/sample 4.png";
+// import image27 from "../../assets/images/Render 6.png";
+// import image28 from "../../assets/images/Sample 8.png";
+// import image29 from "../../assets/images/Render 9PP.png";
+// import image30 from "../../assets/images/sample 4.png";
+// import image31 from "../../assets/images/Render 6.png";
+// import image32 from "../../assets/images/Sample 8.png";
 import zIndex from "@mui/material/styles/zIndex";
+
+const image1 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/w_500,h_300,c_scale/v1740052898/lmkr68a96okzxql9cxga.jpg";
+const image2 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053746/C1PS_kkifwx.jpg";
+const image3 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053747/Interior_v6dmfz.png";
+const image4 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053751/Livingp_wndpmt.png";
+const image5 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053741/112_elly4b.jpg";
+
+const image6 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053749/Render_1_1_x7ndou.png";
+const image7 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053755/Render_4_renjtd.png";
+const image8 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053759/Render_3_sjhrru.png";
+const image9 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053751/Render_2_asbyrf.png";
+const image10 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053761/sample_1_h0qblc.png";
+const image11 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053745/Mini_kitchen_2_zrkvrn.png";
+const image12 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053758/sample_2_xdhgzh.png";
+const image13 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053763/Render_9PP_xesyzb.png";
+const image14 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053765/Sample_6_qdgzwm.png";
+const image15 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053761/Sample_8_a5jw1e.png";
+const image16 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053762/sample_4_z16rvi.png";
+const image17 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053743/Kitchen_Render_dob8mt.jpg";
+const image18 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053756/Render_111_hhaouw.png";
+const image19 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053765/Sample_3_husmsm.jpg";
+const image20 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053742/2P_fkc0wa.jpg";
+const image21 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053761/Sample_8_a5jw1e.png";
+const image22 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053767/Sample_51_f6kpqf.jpg";
+const image23 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053763/Sample_render_nxpzlm.jpg";
+const image24 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053765/Snow_render1_jzstan.jpg";
+const image25 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053763/Render_9PP_xesyzb.png";
+const image26 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053762/sample_4_z16rvi.png";
+const image27 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053753/Render_6_ty0ain.png";
+const image28 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053761/Sample_8_a5jw1e.png";
+const image29 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053763/Render_9PP_xesyzb.png";
+const image30 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053762/sample_4_z16rvi.png";
+const image31 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053765/Sample_6_qdgzwm.png";
+const image32 =
+  "https://res.cloudinary.com/ddxbo66ik/image/upload/v1740053761/Sample_8_a5jw1e.png";
 
 const imagesArray = [
   image1,
