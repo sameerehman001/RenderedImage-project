71c3855687d22f16b689aac602d54c46be505f1d
Samee-Ur-Rehman <sameerehman001>
2025-02-20 20:11:57 +0500

    fixed button designing and other things

--- src/components/buttons/BlackButton.jsx
+++ src/components/buttons/BlackButton.jsx
@@ -2,8 +2,11 @@ import React from "react";
 import styles from "./blackButton.module.css";
 
 const BlackButton = () => {
+  const onClickButton = () => {
+    window.open("https://www.instagram.com/royali_designs/", "_blank");
+  };
   return (
-    <div className={styles.BlackButton}>
+    <div className={styles.BlackButton} onClick={onClickButton}>
       <h4>WATCH OUR REELS</h4>
     </div>
   );
